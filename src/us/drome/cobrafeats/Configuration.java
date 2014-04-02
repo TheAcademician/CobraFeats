16a870e29ebd27f710f5bf647277f08d2cce8640
TheAcademician <TheAcademician@gmail.com>
2014-04-01 19:56:32 -0500

    Begin adding config section for Aesthetic Command Blocks.

--- src/us/drome/cobrafeats/Configuration.java
+++ src/us/drome/cobrafeats/Configuration.java
@@ -8,6 +8,7 @@ public class Configuration {
     private final CobraFeats plugin;
     
     public boolean SILK_TOUCH_SPAWNERS;
+    public boolean AESTHETIC_COMMAND_BLOCKS;
     
     public Configuration(CobraFeats instance) {
         plugin = instance;
@@ -23,6 +24,7 @@ public class Configuration {
         plugin.reloadConfig();
         
         SILK_TOUCH_SPAWNERS = plugin.getConfig().getBoolean("silk-touch-spawners", false);
+        AESTHETIC_COMMAND_BLOCKS = plugin.getConfig().getBoolean("aesthetic-command-blocks", false);
     }
     
     public void reload() {
@@ -34,6 +36,7 @@ public class Configuration {
         settings.add(ChatColor.RED + "-=/" + ChatColor.RESET + "Current Cobra Feature Status" + ChatColor.RED + "\\=-");
         
         settings.add(ChatColor.GOLD + "Silk Touch Spawners: " + (SILK_TOUCH_SPAWNERS ? (ChatColor.GREEN + "Enabled") : (ChatColor.GRAY + "Disabled")));
+        settings.add(ChatColor.GOLD + "Aesthetic Command Blocks: " + (AESTHETIC_COMMAND_BLOCKS ? (ChatColor.GREEN + "Enabled") : (ChatColor.GRAY + "Disabled")));
         
         return settings.toArray(new String[settings.size()]);
     }
